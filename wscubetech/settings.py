bc2bbbb218387ea804809f36df93b4d5e1835caa
ShrinathMateti <134631721+ShrinathMateti@users.noreply.github.com>
2024-06-15 10:23:03 +0530

    Update settings.py

--- wscubetech/settings.py
+++ wscubetech/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-9kk73di%go9i%w*2jw-u(iul5%-r5q&ddv_q!4qh+%dw!$192n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
